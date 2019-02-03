c4b3c902dc255866c0c931fc8b80cea164dabd8d
abby <abbyscheuerman@gmail.com>
2019-02-02 22:28:54 -0500

    Initial Commit

---
+++ onePage.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+          sectionsColor: ['white', 'white', 'white','white', 'white'],
+        });
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
